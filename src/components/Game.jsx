06284752136efea2454ed0bf445e82209b08da57
JyotiDuhan <duhanjyoti@gmail.com>
2024-04-16 18:19:04 +0530

    adding colors

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,25 +1,37 @@
 import React from 'react';
 
 export default function Game(){
-	let count = [1,2,3,4];
-	let rCount = [5,6,7,8];
+	// let count = ['#CD5C5C','#40E0D0','#CCCCFF','#FF7F50'];
+	let count = ['red', 'green', 'blue', 'yellow'];
+	let rCount = ['red', 'green', 'blue', 'yellow'];
 
 	function flipCard(index) {
 		let card = document.getElementById(index);
 		card.classList.toggle('is-flipped');
 	}
 
+	function shuffleArray(array) {
+		let input = array;
+	    for (var i = input.length - 1; i > 0; i--) {
+	        var j = Math.floor(Math.random() * (i + 1));
+	        var temp = input[i];
+	        input[i] = input[j];
+	        input[j] = temp;
+	    }
+	    return input;
+	}
+
 	const cards = count.map((item, index) => 
-		<div key={index} id={item} className='flip-cards' onClick={() => flipCard(item)}>
-			<div className="flip-card-front flip-face">{item}</div>
-			<div className="flip-card-back flip-face">{}</div>
+		<div key={index} id={index} className='flip-cards' onClick={() => flipCard(index)}>
+			<div className="flip-card-front flip-face">{}</div>
+			<div className="flip-card-back flip-face" style={{backgroundColor: item}}>{}</div>
 		</div>
 	);
 
-	const rightCards = rCount.map((item, index) => 
-		<div key={index} id={item} className='flip-cards' onClick={() => flipCard(item)}>
-			<div className="flip-card-front flip-face">{index+1}</div>
-			<div className="flip-card-back flip-face">{}</div>
+	const rightCards = shuffleArray(rCount).map((item, index) => 
+		<div key={item} id={index+4} className='flip-cards' onClick={() => flipCard(index+4)}>
+			<div className="flip-card-front flip-face">{}</div>
+			<div className="flip-card-back flip-face" style={{backgroundColor: item}}>{}</div>
 		</div>
 	);
 
