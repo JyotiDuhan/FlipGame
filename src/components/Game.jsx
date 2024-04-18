11405a986177fe4c25160c3dd920beea89686898
JyotiDuhan <duhanjyoti@gmail.com>
2024-04-18 19:04:21 +0530

    decalre game over changes IP

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, createContext, useContext } from 'react';
+import Winner from './Winner';
 
+export const ContestContext = createContext(null);
 export default function Game(){
+	const [winner, setWinner] = useState('');
 	const [lS, setLs] = useState('');
 	const [rS, setRs] = useState('');
+	const [rCount, setrCount] = useState(shuffleArray(['red', 'green', 'blue', 'yellow']));
 
 	let count = ['red', 'green', 'blue', 'yellow'];
-	let rCount = ['red', 'green', 'blue', 'yellow'];
+	// let rCount = shuffleArray(['red', 'green', 'blue', 'yellow']);
 
 	function flipCard(index, item, side) {
 		let card = document.getElementById(index);
@@ -14,14 +18,28 @@ export default function Game(){
 			setLs(item);
 			let leftList = document.getElementById('left');
 			leftList.classList.add('disable');
-			// remove disable class from right side if exists - ToDo
-			// also, reset right side state if exists - Todo
+			// remove disable class from right side if exists 
+			if(document.getElementById('right').classList.contains('disable')) {
+				document.getElementById('right').classList.remove('disable');
+			}
+			if(rS && rS === lS) {
+				setWinner('Game over.');
+			} else if(rS) { // also, reset right side state if exists 
+				setRs('');
+			}
 		} else if(side === 'r' && !rS) {
 			setRs(item);
 			let rightList = document.getElementById('right');
 			rightList.classList.add('disable');
-			// remove disable class from left side if exists - ToDo
-			// also, reset left side state if exists - Todo
+			// remove disable class from left side if exists 
+			if(document.getElementById('left').classList.contains('disable')) {
+				document.getElementById('left').classList.remove('disable');
+			}
+			if(lS && lS === rS){
+				setWinner('Game over.');
+			} else if(lS) { // also, reset left side state if exists
+				setLs('');
+			}
 		} 
 	}
 
@@ -43,7 +61,7 @@ export default function Game(){
 		</div>
 	);
 
-	const rightCards = shuffleArray(rCount).map((item, index) => 
+	const rightCards = rCount.map((item, index) => 
 		<div key={item} id={index+4} className='flip-cards right-side' onClick={() => flipCard(index+4, item, 'r')}>
 			<div className="flip-card-front flip-face">{}</div>
 			<div className="flip-card-back flip-face" style={{backgroundColor: item}}>{}</div>
@@ -51,9 +69,17 @@ export default function Game(){
 	);
 
 	return (
-		<div className="game-wrap">
-			<div id='left'>{cards}</div>
-			<div id='right'>{rightCards}</div>
-		</div>
+		<ContestContext.Provider value={winner}>
+			<div>
+				{winner 
+					? <Winner />
+					:
+					<div className="game-wrap">
+						<div id='left'>{cards}</div>
+						<div id='right'>{rightCards}</div>
+					</div>
+				}
+			</div>
+		</ContestContext.Provider>
 	)
 }
